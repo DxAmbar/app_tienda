2852245c39b7a8824326a930db8668eb3f079d2d
Misdely Morales <mmoralbr@emeal.nttdata.com>
2023-04-21 18:48:08 -0400

    mostrando productos en el home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ListaProductosService } from 'src/app/services/lista-productos.service';
+import { Productos } from 'src/app/services/productos';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  @Input() productos : Productos[] = [];
+
+    constructor(private productoService: ListaProductosService){}
+
+    ngOnInit(): void {
+      this.getProductos();
+    }
+  
+    getProductos() : void{
+      this.productoService.getProductos().subscribe(
+        (data) => {
+          this.productos = data.products
+          console.log(this.productos);
+        }
+      )
+  
+    }
 
 }
